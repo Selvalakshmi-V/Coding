a08292e0ae821b903a46a3d0b03272fa9db92ea0
Selvalakshmi <vselvalakshmi24@gmail.com>
2018-08-01 17:03:49 +0530

    first commit

---
+++ src/app/errorMessages.ts
@@ -0,0 +1,20 @@
+// export class Recipe {
+//     public name: string;
+//     public description: string;
+//     public imagePath: string;
+  
+//     constructor(name: string, desc: string, imagePath: string) {
+//       this.name = name;
+//       this.description = desc;
+//       this.imagePath = imagePath;
+//     }
+//   }
+
+export class errorMessages {
+    constructor() {}
+    emailRequired = 'Email Field is Required';
+    emailValid = 'Email is not valid';
+    passwordRequired = 'Password Field is required';
+    passwordMatch = ' Password must be 8 character long with at least one character and one special character';    
+}
+
