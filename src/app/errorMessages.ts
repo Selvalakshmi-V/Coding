e383ff42d35331b4d303c1bd747b00b9cfa4fd82
Selvalakshmi <vselvalakshmi24@gmail.com>
2018-08-01 17:06:24 +0530

    first commit

--- src/app/errorMessages.ts
+++ src/app/errorMessages.ts
@@ -1,15 +1,3 @@
-// export class Recipe {
-//     public name: string;
-//     public description: string;
-//     public imagePath: string;
-  
-//     constructor(name: string, desc: string, imagePath: string) {
-//       this.name = name;
-//       this.description = desc;
-//       this.imagePath = imagePath;
-//     }
-//   }
-
 export class errorMessages {
     constructor() {}
     emailRequired = 'Email Field is Required';
